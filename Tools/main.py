b8c40fee29451d810cf338dca17ed2e12205b2a6
Philipp Gampe <philipp.gampe@typo3.org>
2014-12-21 16:07:18 +0100

    [TASK] Implement log file analyzer

--- Tools/main.py
+++ Tools/main.py
@@ -1,25 +1,122 @@
 import urllib2
+import re
+import os.path
+import numpy as np
+import matplotlib.pyplot
 
-def getRecentData(useFtp = True, asObject = False):
-    """Fetches the most recent data from the server. Throws URLError if
+log = '../Data/PA.log'
+log_url = 'rogue-01.informatik.uni-bonn.de/PA.log'
+log_regex = '64 bytes from (\d*\.\d*\.\d*\.\d*): seq=(\d*) ttl=(\d*) time=(\d*\.\d*) ms'
+
+
+def downloadFile(url='', filename=''):
+    if url == '' or filename == '':
+        return
+    d = getFile(url, ftp=False)
+    f = open(filename, 'w')
+    f.write(d)
+    d.close()
+    f.close()
+
+
+def getFile(url='', ftp=True, asObject=False):
+    """
+    Fetches the most recent data from the server. Throws URLError if
     the server is offline or the file can't be found.
-    \param useFTP True if FTP, False if HTTP.
-    \param asObject If True, this method returns an iterable Object.
-    If False it returns a String. Keep in mind that you have to close() the
-    Object after you're done.
-    \returns The recent data as String or Object."""
-    if useFtp:
-        response = urllib2.urlopen('ftp://rogue-01.informatik.uni-bonn.de/PA.log')
+
+    :param url: The URL without a scheme.
+    :param ftp: True if FTP, False if HTTP.
+    :param asObject: If True, this method returns an iterable Object.
+
+    :returns: The recent data as String or Object.
+    """
+    if ftp:
+        response = urllib2.urlopen('ftp://' + url)
     else:
-        response = urllib2.urlopen('http://rogue-01.informatik.uni-bonn.de/PA.log')
+        response = urllib2.urlopen('http://' + url)
     if asObject:
         return response
     result = response.read()
     response.close()
     return result
 
-yourData = getRecentData(asObject=True)
 
-for line in yourData:
-    print(line) # Double linebreaks, because \r\n in file + print()-Linebreak.
-yourData.close()
+def analizeLogFile(f):
+    """
+    :param f: The logfile file object
+    :return: The set data with all data points below each timestamp or False on error
+
+    :type f: file
+    """
+    data = {}
+    line = f.readline()
+    regex = re.compile(log_regex)
+
+    # iterate file until EOF
+    while True:
+        # each time we start with the timestamp
+        try:
+            timestamp = int(line)
+        except ValueError:
+            timestamp = 0
+        if timestamp == 0:
+            if f.readline() == '':
+                return data
+            else:
+                return False
+
+        # next line must start with PING
+        line = f.readline()
+        if line[:4] == 'PING':
+            line = f.readline()
+        else:
+            continue
+
+        # now comes our data set (max 10 pings)
+        currentSet = []
+        for i in range(0, 10):
+            line = f.readline()
+            if line[:2] != '64':
+                break
+            r = regex.match(line)
+            currentSet.append(r.groups())
+        data[timestamp] = currentSet
+
+        # empty line after data block
+        if line != '\n':
+            continue
+
+        # statistics block with three lines in total (ignored)
+        line = f.readline()
+        if line[:3] == '---':
+            f.readline()
+            f.readline()
+        else:
+            continue
+
+        # read the line for next run of the loop
+        line = f.readline()
+
+    return False
+
+
+def plot_log(d):
+    a = 0
+
+
+def main():
+    if not os.path.isfile(log):
+        downloadFile(log_url)
+    f = file(log, 'r')
+    r = analizeLogFile(f)
+    if r == False:
+        f.close()
+        print 'Failed to parse file.'
+        return 1
+
+    plot_log(r)
+
+    f.close()
+    return 0
+
+main()
